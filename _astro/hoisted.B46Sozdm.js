f0dc6e1c31fc94ce0835409f0118615b195b389e
hamrahan-platform <hamrahan.app@gmail.com>
2024-03-20 12:53:34 +0000

    Update from https://github.com/hamrahan-platform/hamrahan-website/commit/809406687a1651c153c0f01eb48c098e485082b3

---
+++ _astro/hoisted.B46Sozdm.js
@@ -0,0 +1,1 @@
+import"./hoisted.BhChhb8F.js";const a=new URLSearchParams(window.location.search),r=Object.fromEntries(a.entries());console.log(r);fetch("//app.hamrahan.app/api/v1/telegram/verifiy");
--- _astro/hoisted.yFlZNF1i.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.QOcMGqPq.js";const a=new URLSearchParams(window.location.search),r=Object.fromEntries(a.entries());console.log(r);fetch("//app.hamrahan.app/api/v1/telegram/verifiy");
