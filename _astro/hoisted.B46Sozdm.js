411358cad17082571b3c83390a664922ffbe62a2
hamrahan-platform <hamrahan.app@gmail.com>
2024-03-20 13:16:35 +0000

    Update from https://github.com/hamrahan-platform/hamrahan-website/commit/9fb42112b7b74653ebaa7b903db798bbe19b37f1

--- _astro/hoisted.B46Sozdm.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.BhChhb8F.js";const a=new URLSearchParams(window.location.search),r=Object.fromEntries(a.entries());console.log(r);fetch("//app.hamrahan.app/api/v1/telegram/verifiy");
